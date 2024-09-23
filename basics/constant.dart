daed80ede27be51b345fc3455653da218016afac
Chetan728 <158229327+Chetan728@users.noreply.github.com>
2024-09-23 07:39:27 +0000

    learn

---
+++ basics/constant.dart
@@ -0,0 +1,13 @@
+void main(){
+  // final , and const 
+  // final = fix value run code
+  // constant = fix value at the initilisation;
+
+  const yourName='chetan';
+  // yourName = 'prince'  nahi kar sakta
+
+  final dateTime = DateTime.now();
+  // kyoki pta nahi kab code run hoga isiliye hamne ise run time constant banaya h 
+  
+
+}
